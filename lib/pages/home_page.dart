5179d7fecef82f6a30307b4b498b68c2d3a37737
Shivdutt <hanuman.9958@gmail.com>
2023-08-23 03:43:33 +0530

    day10 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/models/catelog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import 'package:flutter_application_1/widgets/item_widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    loadData();
+    super.initState();
+  }
+
+  loadData() async {
+    final catelogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decondeData = jsonDecode(catelogJson);
+    var productData = decondeData["products"];
+    // print(productData);
+  }
 
   @override
   Widget build(BuildContext context) {
